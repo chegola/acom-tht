4eaec584d5961fa736bdd656be602586b9200fb0
Unknown <arkhomk@yahoo.com>
2018-11-14 12:57:16 +0700

    Almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'acomTakeHomeTest';
+  randomNumbers = new Array();
   digit = 1;
   startRange = 1;
   endRange = 9;
-  limitOfRange = 9;
   Math: any;
   submitted = false;
+  MAX_RETRY = 1000;
+  noOfRetry = 0;
+
   constructor() {
     this.Math = Math;
   }
@@ -21,22 +24,33 @@ export class AppComponent {
     this.getRandom();
   }
 
+  public doReset(): void {
+    this.randomNumbers = [];
+  }
+
   private getRandom(): void {
-    const a = new Array();
+    this.noOfRetry = 0;
     for (let i = this.startRange ; i <= this.endRange; i++) {
       let isRepeat = true;
-      console.log('i ='  + i);
+      console.log('i = '  + i);
       while (isRepeat) {
         const n = this.getNextRandomNumber();
-        if (a.indexOf(n) === -1) {
-          console.log('random  :' + n.toString());
-          a.push( n.toString());
+        const strNumber = n.toString().padStart(this.digit, '0');
+        if (this.randomNumbers.indexOf(strNumber) === -1) {
+          console.log('random number  :' + strNumber);
+          this.randomNumbers.push(strNumber);
           isRepeat = false;
         } else {
           console.log('Found duplicate number.. re-random' );
           isRepeat = true;
+          this.noOfRetry++;
+          if (this.noOfRetry > this.MAX_RETRY) {
+            isRepeat = false;
+            console.log('Exceed maximum number of retry.' );
+          }
         }
       }
+      this.submitted = false;
     }
     console.log('End');
   }
