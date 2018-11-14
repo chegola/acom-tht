7b0a82bfb06f48d5a813abe527339250ab4a065c
Unknown <arkhomk@yahoo.com>
2018-11-14 13:51:45 +0700

    Add my CV link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'acomTakeHomeTest';
+  title = 'A-Commerce Take Home Test';
   randomNumbers = new Array();
   digit = 1;
   startRange = 1;
